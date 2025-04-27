03724113865e59e9267fddd7821ca0d1523b2821
JorgeEnriqueLN <jotaeln.22@gmail.com>
2025-04-27 19:06:01 -0300

    creación clase TarjetaSube

---
+++ Ejercicios/Ejercicio_1_U2/claseTarjetaSube.py
@@ -0,0 +1,32 @@
+class TarjetaSube:
+    __saldo = int
+    __numero = int
+
+    def __init__(self, sal, nro):
+        self.__saldo = sal
+        self.__numero = nro
+
+    def getSaldo(self):
+        return self.__saldo
+
+    def getNumero(self):
+        return self.__numero
+    
+    def __str__(self):
+        return f"Numero de tarjeta: {self.__numero} \nSaldo: {self.__saldo}"
+     
+    def Pagar_pasaje (self, importe):
+        if self.__saldo >= importe:
+            self.__saldo -= importe
+            return (self.__saldo)
+        else:
+            return False
+        
+    def Cargar_saldo (self, importe):
+        self.__saldo += importe
+        return (self.__saldo)   
+    
+    def Consultar_saldo (self, num):
+        if self.__numero == num:
+            return (self.__saldo)
+    
