b66978734d4b748809c8368dd976f59e674d1dc4
Jorge <jotaeln.22@gmail.com>
2025-04-27 22:39:07 -0300

    instanciación de la clase tarjeta en test

--- Ejercicios/Ejercicio_1_U2/test.py
+++ Ejercicios/Ejercicio_1_U2/test.py
@@ -1,4 +1,7 @@
+from claseTarjetaSube import TarjetaSube
+
 def test():
     for i in range(3):
         saldo= int(input("Ingrese el saldo de la tarjeta: "))
         numero= int(input("Ingrese el numero de la tarjeta: "))
+        tarjeta= TarjetaSube(saldo, numero)
